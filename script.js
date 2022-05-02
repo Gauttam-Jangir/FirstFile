0590cf1ab515c2f020e7f30e33355c086e30c8b9
Gauttam-Jangir <gauttamj49@gmail.com>
2022-05-02 18:17:24 +0530

    first JavaScript commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Hello world program
+var name="Hello world";
+console.log(name);
+
+var a=10;
+var b=20;
+var c="10";
+var name="ABC";
+console.log(a+name);
+console.log(a+b);
+console.log(a+c);
+console.log(a==b);
+console.log(a===c);
+
+window.alert("You just now learnt JavaScript");
